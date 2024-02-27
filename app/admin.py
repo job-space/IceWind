788887fdcddfcfc87a68eae9c14fdc7a40ade745
job-space <132493031+job-space@users.noreply.github.com>
2024-02-28 01:11:00 +0200

    Create admin.py

---
+++ app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import NewsPost, MenuPost, Category, Contact, Franchise, Resume
+
+admin.site.register(NewsPost)
+admin.site.register(MenuPost)
+admin.site.register(Category)
+admin.site.register(Contact)
+admin.site.register(Franchise)
+admin.site.register(Resume)
